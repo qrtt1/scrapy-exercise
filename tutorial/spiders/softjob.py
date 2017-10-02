2adccd168d97bf5df19f217fac3dfd67504303b3
Ching Yi, Chan <chingyichan.tw@gmail.com>
2017-10-02 13:49:25 +0800

    fix bug

--- tutorial/spiders/softjob.py
+++ tutorial/spiders/softjob.py
@@ -6,25 +6,34 @@ import os.path
 class SoftJobSpider(scrapy.Spider):
     name = "softjob"
     base_url = 'https://www.ptt.cc'
-
-    def __init__(self):
-        self.page_count = 0
+    skip_page_uris = ['/bbs/Soft_Job/index1.html', '/bbs/Soft_Job/index.html']
 
     def start_requests(self):
         url = 'https://www.ptt.cc/bbs/Soft_Job/index.html'
-        yield scrapy.Request(url=url, callback=self.parse_list)
+        request = scrapy.Request(url=url, callback=self.parse_next_page)
+        request.meta['page'] = 0
+        yield request
 
     def parse_next_page(self, response):
-        if self.page_count > 10: yield None
-        self.page_count += 1
-        url = self.base_url + response.css('.btn-group-paging a::attr(href)')[1].extract()
-        yield scrapy.Request(url=url, callback=self.parse_list)
+        yield response.follow(response.url, self.parse_list)
+        for url in response.css('.btn-group-paging a::attr(href)').extract():
+            yield response.follow(url, self.parse_list)
+            if url in self.skip_page_uris: continue
+            next_page_url = self.base_url + url
+            print "next_page_url", next_page_url
+            request = scrapy.Request(url=next_page_url, callback=self.parse_next_page)
+            if 'page' not in response.meta:
+                request.meta['page'] = 0
+            else:
+                request.meta['page'] = response.meta['page'] + 1
+
+            if request.meta['page'] <= 5:
+                yield request
 
     def parse_list(self, response):
+        print "parse_list", response.url
         for x in response.css('.r-list-container div a::attr(href)'):
-            url = self.base_url + x.extract()
-            yield scrapy.Request(url=url, callback=self.save_page)
-        yield scrapy.Request(url=response.url, callback=self.parse_next_page)
+            yield response.follow(x, self.save_page)
 
     def save_page(self, response):
         author, board, title, publish_datetime = response.css("#main-content span.article-meta-value::text").extract()
